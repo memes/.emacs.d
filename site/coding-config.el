82b993acc41ae50ccca6baa9b2b16e32a8f0f1c8
Matthew Emes <memes@matthewemes.com>
2011-08-22 14:13:32 -0700

    Added Groovy/Grails support

--- site/coding-config.el
+++ site/coding-config.el
@@ -108,3 +108,12 @@
 	    (local-set-key [f2] 'flymake-goto-prev-error)
 	    (local-set-key [f3] 'flymake-goto-next-error)
 	    ))
+
+;; Groovy/Grails integration
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-hook 'groovy-mode-hook
+	  '(lambda ()
+	     (require 'groovy-electric)
+	     (groovy-electric-mode)))
