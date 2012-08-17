5380531f6593805bb41f7e555124e3aedff1fb24
Matthew Emes <memes@aaxisgroup.com>
2012-08-17 12:14:37 -0700

    Removed obsolete hosts; cookie died in 2001 and I left mercury in 2006

--- site/01it7364lap.el
+++
@@ -1,9 +0,0 @@
-;; Emacs settings for cookie
-;; $Id: 01it7364lap.el 1 2007-10-18 23:15:33Z memes $
-
-
-(setq user-mail-address "memes@mercuryinsurance.com"
-      calendar-latitude +33.9
-      calendar-longitude -117.9
-      calendar-location-name "Brea, CA")
-
