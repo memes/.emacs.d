9401e0a1028cb8c0ba86c68100b71b7ee377a219
Matthew Emes <memes@matthewemes.com>
2014-11-30 21:45:52 -0800

    Updated configuration for coding  - use MELPA packages rather than local elisp scripts  - remove unused modes

--- lisp/coding-config.el
+++ lisp/coding-config.el
@@ -1,141 +1,120 @@
 ;; General coding configuration options
 ;; $Id: coding-config.el 7 2008-06-27 15:21:24Z memes $
 
-;; Patch/diff mode
-(autoload 'diff-mode "diff-mode" "Diff major mode" t)
-(setq diff-switches "-Naur")
-(add-to-list 'auto-mode-alist '("\\.\\(diffs?\\|patch\\|rej\\)\\'" . diff-mode))
+;; Show matching parenthesis where possible
+(show-paren-mode 1)
 
-;; Xref integration - disabled since no JDK <1.5 for amd64
-(defun my-load-xrefactory()
-  (defvar xref-key-binding 'local)
-  (setq exec-path (cons "~/xref" exec-path)
-	load-path (cons "~/xref/emacs" load-path))
-  (load "xrefactory")
-  )
-;;(add-hook 'java-mode-hook 'my-load-xrefactory)
+;; List of hooks to add paredit support
+;;  - add to this list when modes should hook paredit
+(defvar memes-paredit-mode-hooks
+  '(emacs-lisp-mode-hook lisp-mode-hook)
+  "Hooks to include paredit support")
 
-;; Default c-mode settings
-(defun my-c-mode-common-hook ()
+;; Enable paredit for list like modes
+(with-eval-after-load "paredit"
+  ;; Don't override M-r since I use it too often
+  (define-key paredit-mode-map (kbd "<M-r>" nil))
+  ;; Add paredit mode to all hooks in memes-paredit-mode-hooks
+  (mapc (lambda (hook)
+	  (add-hook hook (lambda ()
+			   (paredit-mode +1))))
+	memes-paredit-mode-hooks))
+
+;; Autocomplete package
+(add-to-list 'memes-packages 'auto-complete)
+(with-eval-after-load "auto-complete"
+  (global-auto-complete-mode t))
+
+;; Patch/diff mode if installed on OS
+(cond ((fboundp 'diff-mode)
+       ;; Autoload diff mode for diff/patch like files
+       (autoload 'diff-mode "diff-mode" "Diff major mode" t)
+       (setq diff-switches "-Naur")
+       (add-to-list 'auto-mode-alist '("\\.\\(diffs?\\|patch\\|rej\\)\\'" . diff-mode))))
+
+;; Add flycheck to all supported languages
+(add-to-list 'memes-package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+(add-to-list 'memes-packages 'flycheck)
+(with-eval-after-load "flycheck"
+  (add-hook 'emacs-startup-hook 'global-flycheck-mode))
+
+;; C-mode hook common to all sub-modes
+(defun memes-c-mode-common-hook ()
   (setq c-basic-offset 4)
   (c-set-offset 'substatement-open 0)
   (c-set-offset 'statement-case-open 0)
   (c-set-offset 'case-label '+)
   (setq-default tab-width 4 indent-tabs-mode nil)
   (turn-on-auto-fill)
-  (flyspell-prog-mode)
-  (message "my-c-mode-common-hook function executed"))
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+  (flyspell-prog-mode))
+(add-hook 'c-mode-common-hook 'memes-c-mode-common-hook)
 
-;; Set linux mode
+;; Define a set of path/c-style option pairs
+(setq memes-c-style-alist
+      '(("~/dev/kernel" . "linux-tabs-only")
+	(nil . "k&r")))
+
+;; Function to pick a c-style based on filepath
+(defun memes-choose-c-style ()
+  "Choose a C-style based on filepath of buffer"
+  (let ((style
+	 (assoc-default buffer-file-name memes-c-style-alist
+			(lambda (pattern path)
+			  (or (not pattern)
+			      (and (stringp path)
+				   (string-match (expand-file-name pattern)
+						 path))))
+			;; Add a default in case there are no matches
+			'(nil . "k&r"))))
+    (cond
+     ((stringp style) (c-set-style style))
+     ((functionp style) (style)))))
+(add-hook 'c-mode-hook 'memes-choose-c-style)
+
+;; Setup Linux style - mostly from Documentation/CodingStyle
 (defun c-lineup-arglist-tabs-only (ignored)
   "Line up argument lists by tabs, not spaces"
   (let* ((anchor (c-langelem-pos c-syntactic-element))
 	 (column (c-langelem-2nd-pos c-syntactic-element))
 	 (offset (- (1+ column) anchor))
-        (steps (floor offset c-basic-offset)))
+	 (steps (floor offset c-basic-offset)))
     (* (max steps 1)
        c-basic-offset)))
-
-(add-hook 'c-mode-hook
-          (lambda ()
-            (let ((filename (buffer-file-name)))
-              ;; Enable kernel mode for the appropriate files
-              (when (and filename
-                         (string-match "~/dev/kernel" filename))
-                (setq indent-tabs-mode t)
-                (c-set-style "linux")
-                (c-set-offset 'arglist-cont-nonempty
-                              '(c-lineup-gcc-asm-reg
-                                c-lineup-arglist-tabs-only))))))
-
-
-;; Show matching parenthesis
-(show-paren-mode t)
-
-;; Recognise Ant and Jikes errors
-(require 'compile)
-(setq compilation-error-regexp-alist 
-      (append (list
-	       ;; works for jikes
-	       '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):\\([0-9]+\\):[0-9]+:[0-9]+:" 1 2 3)
-	       ;; works for javac
-	       '("^\\s-*\\[[^]]*\\]\\s-*\\(.+\\):\\([0-9]+\\):" 1 2))
-	      compilation-error-regexp-alist))
-
-;; C# Mode - from Beagle's HACKING doc
-(defun poor-mans-csharp-mode ()
-  (java-mode)
-  (setq mode-name "C#")
-  (set-variable 'tab-width 8)
-  (set-variable 'indent-tabs-mode t)
-  (set-variable 'c-basic-offset 8)
-  (c-set-offset 'inline-open 0)
-  (c-set-offset 'case-label 0)
-)
-
-(setq auto-mode-alist (append '(("\\.cs\\'" . poor-mans-csharp-mode))
-			      auto-mode-alist))
-
-;; Flymake configuration - add integration for python and use Ant for Java
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		       'flymake-create-temp-inplace))
-	   (local-file (file-relative-name
-			temp-file
-			(file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-  (setq flymake-allowed-file-name-masks
-        '(("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-init)
-;;          ("\\.xml\\'" flymake-xml-init)
-;;          ("\\.html?\\'" flymake-xml-init)
-          ("\\.cs\\'" flymake-simple-make-init)
-          ("\\.p[ml]\\'" flymake-perl-init)
-          ("\\.php[345]?\\'" flymake-php-init)
-          ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup)
-;;          ("\\.java\\'" flymake-simple-ant-java-init flymake-simple-java-cleanup)
-          ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup)
-          ("\\.tex\\'" flymake-simple-tex-init)
-          ("\\.idl\\'" flymake-simple-make-init))))
-
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-;; Python integration 
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (unless (eq buffer-file-name nil) (flymake-mode 1)) ; don't invoke flymake in interpreter buffers
-	    (local-set-key [f2] 'flymake-goto-prev-error)
-	    (local-set-key [f3] 'flymake-goto-next-error)
-	    ))
+(defconst linux-tabs-only
+  '("linux"
+    (c-offsets-alist
+     (arglist-cont-nonempty
+      c-lineup-gcc-asm-reg
+      c-lineup-arglist-tabs-only)))
+  "Linux CodingStyle recommendations")
+(c-add-style "linux-tabs-only" linux-tabs-only)
 
 ;; Groovy/Grails integration
-(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
-(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
-(add-hook 'groovy-mode-hook
-	  '(lambda ()
-	     (require 'groovy-electric)
-	     (groovy-electric-mode)))
+(add-to-list 'memes-packages 'groovy-mode)
+(with-eval-after-load "groovy-mode"
+  (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+  (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+  (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+  (add-hook 'groovy-mode-hook
+	    '(lambda ()
+	       (require 'groovy-electric)
+	       (groovy-electric-mode))))
 
-;; Lua mode
-(autoload 'lua-mode "lua-mode" "Lua editing mode" t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+;; Lua mode if installed OS package
+(cond ((fboundp 'lua-mode)
+       (autoload 'lua-mode "lua-mode" "Lua editing mode" t)
+       (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+       (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))))
 
-;; Use Octave for .m files in preference to MATLAB
-(setq octave-block-offset 4)
-(setq auto-mode-alist (cons '("\\.m\\'" . octave-mode) auto-mode-alist))
+;; Use Octave for .m files in preference to MATLAB when OS package is installed
+(cond ((fboundp 'octave-mode)
+       (setq octave-block-offset 4)
+       (setq auto-mode-alist (cons '("\\.m\\'" . octave-mode) auto-mode-alist))))
 
 ;; Load scala from package
-(add-to-list 'memes-package-archives '("melpa" . "http://stable.melpa.org/packages/"))
 (add-to-list 'memes-packages 'scala-mode2)
 
 ;; Clojure package
 (add-to-list 'memes-packages 'clojure-mode)
-
-;; Enable paredit for list like modes
-(when (fboundp 'paredit-mode)
-  (mapc (lambda (hook)
-	  (add-hook hook (lambda () (paredit-mode +1))))
-	'(emacs-lisp-mode-hook lisp-mode-hook clojure-mode-hook)))
+(with-eval-after-load "clojure-mode"
+  (add-to-list 'memes-paredit-mode-hooks clojure-mode-hook))
