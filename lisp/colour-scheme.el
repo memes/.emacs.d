be761f4f9e51c04fa5ab7b623052b6f1700ef234
Matthew Emes <memes@matthewemes.com>
2014-11-30 16:54:56 -0800

    Theme is set via custom variable; font-lock is enabled during hook

--- lisp/colour-scheme.el
+++ lisp/colour-scheme.el
@@ -5,15 +5,18 @@
 (add-to-list 'memes-package-archives '("melpa" . "http://stable.melpa.org/packages/"))
 (add-to-list 'memes-packages 'solarized-theme)
 
+(defvar memes-preferred-theme 'solarized-dark
+  "Preferred theme to use")
+
+;; Register a hook to load a theme and turn on font decorations
+;;  - do in startup hook so that the theme can be downloaded and installed from
+;;    MELPA if necessary
 (defun memes-set-theme ()
-    (load-theme 'solarized-dark t))
+  (load-theme memes-preferred-theme t)
+  (cond ((fboundp 'global-font-lock-mode)
+	 ;; Turn on font-lock in all modes that support it
+	 (global-font-lock-mode t)
+	 ;; Maximum colours
+	 (setq font-lock-maximum-decoration t))
+	))
 (add-hook 'emacs-startup-hook 'memes-set-theme)
-
-
-;; Colour syntax highlighting
-(cond ((fboundp 'global-font-lock-mode)
-       ;; Turn on font-lock in all modes that support it
-       (global-font-lock-mode t)
-       ;; Maximum colours
-       (setq font-lock-maximum-decoration t))
-      )
