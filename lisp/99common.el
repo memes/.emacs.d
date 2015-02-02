3361415e3d520a43c58c94fe9d764392bbb56f78
Matthew Emes <memes@ten-us.com>
2015-02-02 10:30:11 -0800

    Add yasnippet to 99common

--- lisp/99common.el
+++ lisp/99common.el
@@ -1,11 +1,18 @@
 ;; Settings common to all hosts to be executed last
 ;; $Id: 98common.el 1 2007-10-18 23:15:33Z memes $
 
+;; Use yasnippet
+(add-to-list 'memes-packages 'yasnippet)
+(with-eval-after-load "yasnippet"
+  (yas-global-mode 1))
+
 ;; Enable auto-complete if present
 ;; Autocomplete package
 (add-to-list 'memes-packages 'auto-complete)
 (with-eval-after-load "auto-complete"
-  (global-auto-complete-mode t))
+  (global-auto-complete-mode t)
+  (ac-set-trigger-key "TAB")
+  (ac-set-trigger-key "<tab>"))
 
 ;; Disable backtrace on error
 (setq debug-on-error nil)
