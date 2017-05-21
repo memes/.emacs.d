e315378bc875cfca67df7be9f783340a2131348b
Matthew Emes <memes@matthewemes.com>
2017-05-21 09:57:13 -0700

    ELPA: Add GNU elpa packages back for Emacs 24.x

--- lisp/00common.el
+++ lisp/00common.el
@@ -139,7 +139,7 @@
 (defvar memes-packages ()
   "List of packages to be installed in 'after-init-hook'.")
 
-(defvar memes-package-archives '(("melpa" . "https://melpa.org/packages/"))
+(defvar memes-package-archives '(("melpa" . "https://melpa.org/packages/") ("gnu" . "https://elpa.gnu.org/packages/"))
   "List of package archive definitions to add to system.
 Defaults to MELPA packages.")
 
